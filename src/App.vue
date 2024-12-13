4709d2af5e566ec5e93d922df90bb03f8138ec01
yurui <ttttttesla0214@gmail.com>
2024-12-13 22:54:42 +0800

    feat: vue学习简单轮播图实现

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed, reactive, ref } from 'vue';
+
+
+
+const value1 = ref(0)
+
+function jump(value) {
+  value1.value = value
+  console.log(value1.value)
+}
+
+</script>
+
+<template>
+  <img :src="`/${value1}.webp`" alt="图片">
+
+  <ul>
+    <li v-for="value in 4">
+      <a href="#" @click.prevent="jump(value)">{{ value }}</a>
+    </li>
+  </ul>
+
+</template>
+
+<style scoped>
+
+</style>
